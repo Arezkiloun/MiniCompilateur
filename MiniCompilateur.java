491cf87d9a8377b39abd4e3587f97de04b0cba78
Arezkiloun <lounisarezki91@gmail.com>
2025-12-04 20:10:35 +0100

    Add files via upload

--- MiniCompilateur.java
+++ MiniCompilateur.java
@@ -4,24 +4,24 @@ public class MiniCompilateur {
 
     public static void main(String[] args) {
 
-        System.out.println("=== MINI COMPILATEUR (Lexical + Syntaxique) ===");
+        System.out.println("MINI COMPILATEUR ");
 
       
         String chemin = "C:\\Users\\DELL\\OneDrive\\Desktop\\projet compl\\TestSwitch.java";
 
         
 
-        System.out.println("\n=== ETAPE 1 : ANALYSE LEXICALE ===\n");
+        System.out.println("\n ANALYSE LEXICALEn");
         AnalyseurLexical.analyser(chemin);
 
         // Récupération des tokens
         List<Token> tokens = AnalyseurLexical.getTokens();
 
         
-        System.out.println("\n=== ETAPE 2 : ANALYSE SYNTAXIQUE (switch/case) ===\n");
+        System.out.println("\n ANALYSE SYNTAXIQUE \n");
         AnalyseurSyntaxiqueSwitch a = new AnalyseurSyntaxiqueSwitch(tokens);
         a.analyserProgramme();
 
-        System.out.println("\n=== FIN DU MINI COMPILATEUR ===");
+        System.out.println("\n FIN DU MINI COMPILATEUR ");
     }
 }
